cbea0c50fa84a0eee51a59a2096a728ad2300548
Sean Dunagan <dunagan5887@gmail.com>
2016-01-22 02:56:19 -0600

    Adding span and delta functionality to SymbolData class

--- stocks/map_reduce/symbolData.py
+++ stocks/map_reduce/symbolData.py
@@ -2,6 +2,51 @@ class SymbolData:
     
     def __init__(self, symbol):
         self.symbol = symbol
+        self.deltas = {}
+        self.spans = {}
+
+    def setTodayPrice(self, today_price):
+        self.today_price = today_price
+        return self
+
+    def getTodayPrice(self):
+        return self.today_price
+
+    def initializeSpanByCode(self, code):
+        self.spans[code] = []
+        return self
+
+    def addSpanValueByCode(self, code, price):
+        self.spans[code].append(price)
+        return self
+
+    def getPercentageDeltaOffSpanAverage(self, span_code, price_to_compare):
+        span_average = self.getSpanAverageByCode(span_code)
+        delta = price_to_compare - span_average
+        delta_percentage = delta / span_average
+        return delta_percentage
+
+    def getSpanAverageByCode(self, code):
+        span = self.spans[code]
+        span_count = len(span)
+        span_sum = float(sum(span))
+        span_average = span_sum / span_count
+        return span_average
+
+    def initializeDeltaByCode(self, code):
+        self.deltas[code] = 0.0
+        return self
+
+    def setDeltaBefore(self, code, price):
+        self.deltas[code] -= price
+        return self
+
+    def setDeltaAfter(self, code, price):
+        self.deltas[code] += price
+        return self
+
+    def getDeltaByCode(self, code):
+        return self.deltas[code]
     
 class SymbolDataCollection:
     
@@ -14,8 +59,10 @@ class SymbolDataCollection:
     def addSymbolToCollection(self, symbol):
         if not(self.isSymbolInCollection(symbol)):
             self.symbols_dict[symbol] = SymbolData(symbol)
-            
+
+
     def getSymbolData(self, symbol):
+        """ :rtype: SymbolData|None """
         if self.isSymbolInCollection(symbol):
             return self.symbols_dict[symbol]
         return None
