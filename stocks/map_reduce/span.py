928e06f5f1f148683db90fec42719df2f9e3b863
Sean Dunagan <dunagan5887@gmail.com>
2016-01-25 13:19:12 -0600

    Refactoring SpanUnit classes to allow for mapping labels to units as opposed to assigning codes to units

--- stocks/map_reduce/span.py
+++ stocks/map_reduce/span.py
@@ -4,18 +4,32 @@ class Span:
 
     def __init__(self, code):
         self.code = code
+        self.label_to_unit_index_mapping = {}
         self.units = OrderedDict()
 
-    def addSpanUnit(self, unit_code, close_price = None, open_price = None, high_price = None, low_price = None, delta = None, delta_percentage = None):
-        newSpanUnit = SpanUnit(unit_code, close_price, open_price, high_price, low_price, delta, delta_percentage)
-        self.units[unit_code] = newSpanUnit
+    def addSpanUnit(self, unit_labels_list=None, close_price = None, open_price = None, high_price = None, low_price = None, delta = None, delta_percentage = None):
+        newSpanUnit = SpanUnit(close_price, open_price, high_price, low_price, delta, delta_percentage)
+        unit_index = len(self.units)
+        self.units[unit_index] = newSpanUnit
+        for label in unit_labels_list:
+            if not(label in self.label_to_unit_index_mapping):
+                self.label_to_unit_index_mapping[label] = list()
+            self.label_to_unit_index_mapping[label].append(unit_index)
         return self
 
-    def getSpanUnitByCode(self, unit_code):
-        if unit_code in self.units:
-            spanUnit = self.units[unit_code]
-            return spanUnit
-        return None
+    def getSpanUnitsByLabel(self, units_label):
+        """
+        :param string units_label:
+        :return: dict
+        """
+        if not(units_label in self.label_to_unit_index_mapping):
+            return None
+        dict_of_span_units = {}
+        for unit_index in self.label_to_unit_index_mapping[units_label]:
+            spanUnit = self.units[unit_index]
+            dict_of_span_units[unit_index] = spanUnit
+
+        return dict_of_span_units
 
     def getSpanDelta(self):
         if len(self.units) == 0:
@@ -42,8 +56,7 @@ class Span:
         return close_price_average
 
 class SpanUnit:
-    def __init__(self, code, close_price = None, open_price = None, high_price = None, low_price = None, delta = None, delta_percentage = None):
-        self.code = code
+    def __init__(self, close_price = None, open_price = None, high_price = None, low_price = None, delta = None, delta_percentage = None):
         self.high_price = high_price
         self.low_price = low_price
         self.open_price = open_price
--- stocks/map_reduce/test_span.py
+++ stocks/map_reduce/test_span.py
@@ -15,41 +15,42 @@ class TestSpan(unittest.TestCase):
         testSpan = Span(span_code)
         span_delta = testSpan.getSpanDelta()
         self.assertIsNone(span_delta)
-        expected_first_unit_code = 'first_unit'
+        first_unit_label = 'first_unit'
+        second_unit_label = 'second_unit'
+        third_unit_label = 'third_unit'
+        span_test_one_label = 'span_test_one'
+        span_test_two_label = 'span_test_two'
+        expected_first_unit_labels = [first_unit_label, span_test_one_label]
         expected_first_unit_open_price = 100.0
         expected_first_unit_close_price = 95.0
         expected_first_unit_high_price = 110.0
         expected_first_unit_low_price = 900.0
         expected_first_unit_delta = 5
         expected_first_unit_delta_percentage = .04
-        expected_second_unit_code = 'second_unit'
+        expected_second_unit_labels = [second_unit_label, span_test_two_label, span_test_one_label]
         expected_second_unit_open_price = 95.0
         expected_second_unit_close_price = 93.0
         expected_second_unit_high_price = 98.0
         expected_second_unit_low_price = 90.0
         expected_second_unit_delta = -2
         expected_second_unit_delta_percentage = -0.021052632
-        testSpan.addSpanUnit(expected_first_unit_code, expected_first_unit_close_price, expected_first_unit_open_price, expected_first_unit_high_price, expected_first_unit_low_price, expected_first_unit_delta, expected_first_unit_delta_percentage)
-        testSpan.addSpanUnit(expected_second_unit_code, expected_second_unit_close_price, expected_second_unit_open_price, expected_second_unit_high_price, expected_second_unit_low_price, expected_second_unit_delta, expected_second_unit_delta_percentage)
-        testFirstSpanUnit = testSpan.getSpanUnitByCode(expected_first_unit_code)
-        self.assertEqual(expected_first_unit_code, testFirstSpanUnit.code)
+        testSpan.addSpanUnit(expected_first_unit_labels, expected_first_unit_close_price, expected_first_unit_open_price, expected_first_unit_high_price, expected_first_unit_low_price, expected_first_unit_delta, expected_first_unit_delta_percentage)
+        testSpan.addSpanUnit(expected_second_unit_labels, expected_second_unit_close_price, expected_second_unit_open_price, expected_second_unit_high_price, expected_second_unit_low_price, expected_second_unit_delta, expected_second_unit_delta_percentage)
+        # Test the return for the first unit label
+        test_first_unit_label_dict = testSpan.getSpanUnitsByLabel(first_unit_label)
+        self.assertEqual(len(test_first_unit_label_dict), 1)
+        test_first_span_unit_index = test_first_unit_label_dict.items()[0][0]
+        testFirstSpanUnit = test_first_unit_label_dict.items()[0][1]
+        self.assertEqual(test_first_span_unit_index, 0)
         self.assertEqual(expected_first_unit_open_price, testFirstSpanUnit.open_price)
         self.assertEqual(expected_first_unit_close_price, testFirstSpanUnit.close_price)
         self.assertEqual(expected_first_unit_high_price, testFirstSpanUnit.high_price)
         self.assertEqual(expected_first_unit_low_price, testFirstSpanUnit.low_price)
         self.assertEqual(expected_first_unit_delta, testFirstSpanUnit.delta)
         self.assertEqual(expected_first_unit_delta_percentage, testFirstSpanUnit.delta_percentage)
-        testSecondSpanUnit = testSpan.getSpanUnitByCode(expected_second_unit_code)
-        self.assertEqual(expected_second_unit_code, testSecondSpanUnit.code)
-        self.assertEqual(expected_second_unit_open_price, testSecondSpanUnit.open_price)
-        self.assertEqual(expected_second_unit_close_price, testSecondSpanUnit.close_price)
-        self.assertEqual(expected_second_unit_high_price, testSecondSpanUnit.high_price)
-        self.assertEqual(expected_second_unit_low_price, testSecondSpanUnit.low_price)
-        self.assertEqual(expected_second_unit_delta, testSecondSpanUnit.delta)
-        self.assertEqual(expected_second_unit_delta_percentage, testSecondSpanUnit.delta_percentage)
-        testThirdSpanUnit = testSpan.getSpanUnitByCode('third_unit')
+        # Test retrieving a unit which has not been added
+        testThirdSpanUnit = testSpan.getSpanUnitsByLabel('third_unit')
         self.assertIsNone(testThirdSpanUnit)
-        expected_third_unit_code = 'third_unit'
         expected_third_unit_open_price = 93.0
         expected_third_unit_close_price = 88.0
         expected_third_unit_high_price = 97.0
@@ -57,7 +58,50 @@ class TestSpan(unittest.TestCase):
         expected_third_unit_delta = -5
         expected_third_unit_delta_percentage = -0.053763441
         expected_close_price_average = 92.0
-        testThirdSpanUnit = testSpan.addSpanUnit(expected_third_unit_code, expected_third_unit_close_price, expected_third_unit_open_price, expected_third_unit_high_price, expected_third_unit_low_price, expected_third_unit_delta, expected_third_unit_delta_percentage)
+        expected_third_unit_labels = [third_unit_label, span_test_two_label]
+        testThirdSpanUnit = testSpan.addSpanUnit(expected_third_unit_labels, expected_third_unit_close_price, expected_third_unit_open_price, expected_third_unit_high_price, expected_third_unit_low_price, expected_third_unit_delta, expected_third_unit_delta_percentage)
+        # Test the return for the span test one label
+        test_span_one_unit_label_dict = testSpan.getSpanUnitsByLabel(span_test_one_label)
+        self.assertEqual(len(test_span_one_unit_label_dict), 2)
+        test_span_one_first_unit_index = test_span_one_unit_label_dict.items()[0][0]
+        self.assertEqual(test_span_one_first_unit_index, 0)
+        testSpanOneFirstUnit = test_span_one_unit_label_dict.items()[0][1]
+        self.assertEqual(expected_first_unit_open_price, testSpanOneFirstUnit.open_price)
+        self.assertEqual(expected_first_unit_close_price, testSpanOneFirstUnit.close_price)
+        self.assertEqual(expected_first_unit_high_price, testSpanOneFirstUnit.high_price)
+        self.assertEqual(expected_first_unit_low_price, testSpanOneFirstUnit.low_price)
+        self.assertEqual(expected_first_unit_delta, testSpanOneFirstUnit.delta)
+        self.assertEqual(expected_first_unit_delta_percentage, testSpanOneFirstUnit.delta_percentage)
+        test_span_one_second_unit_index = test_span_one_unit_label_dict.items()[1][0]
+        self.assertEqual(test_span_one_second_unit_index, 1)
+        testSpanOneSecondUnit = test_span_one_unit_label_dict.items()[1][1]
+        self.assertEqual(expected_second_unit_open_price, testSpanOneSecondUnit.open_price)
+        self.assertEqual(expected_second_unit_close_price, testSpanOneSecondUnit.close_price)
+        self.assertEqual(expected_second_unit_high_price, testSpanOneSecondUnit.high_price)
+        self.assertEqual(expected_second_unit_low_price, testSpanOneSecondUnit.low_price)
+        self.assertEqual(expected_second_unit_delta, testSpanOneSecondUnit.delta)
+        self.assertEqual(expected_second_unit_delta_percentage, testSpanOneSecondUnit.delta_percentage)
+        # Test the return for the span test two label
+        test_span_two_unit_label_dict = testSpan.getSpanUnitsByLabel(span_test_two_label)
+        self.assertEqual(len(test_span_two_unit_label_dict), 2)
+        test_span_two_first_unit_index = test_span_two_unit_label_dict.items()[0][0]
+        self.assertEqual(test_span_two_first_unit_index, 1)
+        testSpanTwoFirstUnit = test_span_two_unit_label_dict.items()[0][1]
+        self.assertEqual(expected_second_unit_open_price, testSpanTwoFirstUnit.open_price)
+        self.assertEqual(expected_second_unit_close_price, testSpanTwoFirstUnit.close_price)
+        self.assertEqual(expected_second_unit_high_price, testSpanTwoFirstUnit.high_price)
+        self.assertEqual(expected_second_unit_low_price, testSpanTwoFirstUnit.low_price)
+        self.assertEqual(expected_second_unit_delta, testSpanTwoFirstUnit.delta)
+        self.assertEqual(expected_second_unit_delta_percentage, testSpanTwoFirstUnit.delta_percentage)
+        test_span_two_second_unit_index = test_span_two_unit_label_dict.items()[1][0]
+        self.assertEqual(test_span_two_second_unit_index, 2)
+        testSpanTwoSecondUnit = test_span_two_unit_label_dict.items()[1][1]
+        self.assertEqual(expected_third_unit_open_price, testSpanTwoSecondUnit.open_price)
+        self.assertEqual(expected_third_unit_close_price, testSpanTwoSecondUnit.close_price)
+        self.assertEqual(expected_third_unit_high_price, testSpanTwoSecondUnit.high_price)
+        self.assertEqual(expected_third_unit_low_price, testSpanTwoSecondUnit.low_price)
+        self.assertEqual(expected_third_unit_delta, testSpanTwoSecondUnit.delta)
+        self.assertEqual(expected_third_unit_delta_percentage, testSpanTwoSecondUnit.delta_percentage)
         test_close_price_average = testSpan.getSpanCloseAverage()
         self.assertEqual(expected_close_price_average, test_close_price_average)
         expected_span_delta = -12.0
